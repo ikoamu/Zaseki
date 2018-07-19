9fc61bcaca7f8e748f1016090fc6fc7c50c6afc7
miura <akane.miura@esm.co.jp>
2018-07-19 13:52:38 +0900

    ダミーデータを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -30,3 +30,48 @@ VALUES (
 , '3333'
 )
 ;
+INSERT INTO dummymember (
+  name
+, furigana
+, division
+, floor
+, extensionnumber
+)
+VALUES (
+  'piyo　fuga'
+, 'piyo'
+, 'ITS'
+, '4F北'
+, '4444'
+)
+;
+INSERT INTO dummymember (
+  name
+, furigana
+, division
+, floor
+, extensionnumber
+)
+VALUES (
+  'hoge　fuga'
+, 'hoge'
+, 'ITS'
+, '4F南'
+, '4455'
+)
+;
+INSERT INTO dummymember (
+  name
+, furigana
+, division
+, floor
+, extensionnumber
+)
+VALUES (
+  'fuga　piyo'
+, 'fuga'
+, '金融'
+, '2F南'
+, '2222'
+)
+;
