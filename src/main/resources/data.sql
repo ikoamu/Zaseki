2469c0bd478e6305724e68643fbdd29b3fad5703
miura <akane.miura@esm.co.jp>
2018-07-20 09:40:57 +0900

    1つのINSERT文で複数件登録するように修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,60 +14,28 @@ VALUES (
 , '1階南'
 , '1111'
 )
-;
-INSERT INTO dummymember (
-  name
-, furigana
-, division
-, floor
-, extensionnumber
-)
-VALUES (
+, (
   'hoge　piyo'
 , 'hoge'
 , '医療'
 , '3F南'
 , '3333'
 )
-;
-INSERT INTO dummymember (
-  name
-, furigana
-, division
-, floor
-, extensionnumber
-)
-VALUES (
+, (
   'piyo　fuga'
 , 'piyo'
 , 'ITS'
 , '4F北'
 , '4444'
 )
-;
-INSERT INTO dummymember (
-  name
-, furigana
-, division
-, floor
-, extensionnumber
-)
-VALUES (
+, (
   'hoge　fuga'
 , 'hoge'
 , 'ITS'
 , '4F南'
 , '4455'
 )
-;
-INSERT INTO dummymember (
-  name
-, furigana
-, division
-, floor
-, extensionnumber
-)
-VALUES (
+, (
   'fuga　piyo'
 , 'fuga'
 , '金融'
