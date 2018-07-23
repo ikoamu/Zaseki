a177fac4b4fe39599e838238bbb99d21561884d8
miura <akane.miura@esm.co.jp>
2018-07-23 09:51:57 +0900

    SQL文のフォーマットを統一

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS dummymember (
-  id serial PRIMARY KEY,
-  name varchar(20) NOT NULL,
-  furigana varchar(20) NOT NULL,
-  division varchar(20) NOT NULL,
-  floor varchar(20) NOT NULL,
-  extensionnumber char(4) NOT NULL
-);
+  id serial PRIMARY KEY
+, name varchar(20) NOT NULL
+, furigana varchar(20) NOT NULL
+, division varchar(20) NOT NULL
+, floor varchar(20) NOT NULL
+, extensionnumber char(4) NOT NULL
+)
+;
