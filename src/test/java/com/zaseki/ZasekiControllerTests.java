645e386e86a142d7efe0b6ca0ee621833a388b3b
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-07-25 12:35:15 +0900

    SpringJUnit4ClassRunnerからSpringRunnerに変更

--- src/test/java/com/zaseki/ZasekiControllerTests.java
+++ src/test/java/com/zaseki/ZasekiControllerTests.java
@@ -9,15 +9,15 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ZasekiControllerTests {
   private MockMvc mvc;
-  
+
   @InjectMocks
   private ZasekiController zasekiController;
 
