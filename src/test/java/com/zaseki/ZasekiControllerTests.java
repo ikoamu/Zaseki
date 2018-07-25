552362eaeda612d95fb7ddc94c59b394a1ef49df
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-07-25 12:42:07 +0900

    @WebMvcTest、@MockBeanを用いるように修正

--- src/test/java/com/zaseki/ZasekiControllerTests.java
+++ src/test/java/com/zaseki/ZasekiControllerTests.java
@@ -3,31 +3,22 @@ package com.zaseki;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest
 public class ZasekiControllerTests {
+  @Autowired
   private MockMvc mvc;
 
-  @InjectMocks
-  private ZasekiController zasekiController;
-
-  @Mock
+  @MockBean
   private MemberRepository repository;
-
-  @Before
-  public void setUp() {
-    mvc = MockMvcBuilders.standaloneSetup(this.zasekiController).build();
-  }
 
   @Test
   public void test() throws Exception {
