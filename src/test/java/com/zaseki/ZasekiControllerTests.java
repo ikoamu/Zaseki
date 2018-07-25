b5af473e67676e82423374c75715a30c3684411a
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-07-25 11:00:18 +0900

    ZasekiControllerTestの修正
    テストが通るようになりました。

--- src/test/java/com/zaseki/ZasekiControllerTests.java
+++ src/test/java/com/zaseki/ZasekiControllerTests.java
@@ -8,38 +8,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@WebAppConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ZasekiControllerTests {
-  @Autowired
-  private WebApplicationContext wac;
-
+  private MockMvc mvc;
+  
   @InjectMocks
   private ZasekiController zasekiController;
 
   @Mock
   private MemberRepository repository;
 
-  private MockMvc mockMvc;
-
   @Before
   public void setUp() {
-    repository.findAll();
-    mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    mvc = MockMvcBuilders.standaloneSetup(this.zasekiController).build();
   }
 
   @Test
   public void test() throws Exception {
-    mockMvc.perform(get("/member").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    mvc.perform(get("/member")).andExpect(status().isOk());
   }
 }
