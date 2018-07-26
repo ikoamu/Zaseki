d5d517f8723c069ea1b652ee697beb66b95f09e1
miura <akane.miura@esm.co.jp>
2018-07-26 10:25:57 +0900

    データ更新機能の実装
    PATCHリクエストで更新できるようにした。
    データはJSON形式、IDをパラメータで指定

--- src/main/java/com/zaseki/ZasekiController.java
+++ src/main/java/com/zaseki/ZasekiController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,6 +46,12 @@ public class ZasekiController {
   @DeleteMapping(value = "member")
   public void deleteMember(@RequestParam Integer id) {
     repository.deleteById(id);
+  }
+  
+  @PatchMapping(value = "member", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public void updateMember(@RequestParam Integer id, @RequestBody Member member) {
+    member.setId(id);
+    repository.save(member);
   }
 
   private List<Member> findAllMembers() {
