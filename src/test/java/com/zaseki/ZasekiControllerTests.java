43379af66b3b7dbefea6205ed037b4b8c454e456
shotaro.aoki <shotaro.aoki@ems.co.jp>
2018-07-26 12:57:36 +0900

    @WebMvcTestアノテーションのカッコ内を削除

--- src/test/java/com/zaseki/ZasekiControllerTests.java
+++ src/test/java/com/zaseki/ZasekiControllerTests.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = ZasekiController.class)
+@WebMvcTest
 public class ZasekiControllerTests {
   @Autowired
   private MockMvc mvc;
