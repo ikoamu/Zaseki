12d410d665e93293fdde59186e20ee5c05f0e6e0
miura <akane.miura@esm.co.jp>
2018-07-30 10:04:24 +0900

    src/test/resources内のschema.sqlを削除

--- src/test/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS member (
-  id serial PRIMARY KEY
-, name varchar(20) NOT NULL
-, furigana varchar(20) NOT NULL
-, division varchar(20) NOT NULL
-, floor varchar(20) NOT NULL
-, extensionnumber char(4) NOT NULL
-)
-;
